7192d470880ea665f33d5886d4b328de33ff3790
Odilon Garcez <odilon.garcez.moraes@gmail.com>
2021-07-11 21:46:41 -0300

    feat: configure connection mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [BlogModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/blog', {
+      retryDelay: 500,
+      retryAttempts: 3,
+      useNewUrlParser: true,
+    }),
+    BlogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
